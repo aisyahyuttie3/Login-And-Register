ef4b42e74585a0f7fb66856f1464516fbc59b3dc
aisyahyuttie3 <93245233+aisyahyuttie3@users.noreply.github.com>
2021-10-27 22:14:22 +0700

    Login And Register

---
+++ app/src/main/java/com/example/loginandregister/api/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.example.loginandregister.api;
+
+import com.example.loginandregister.model.login.Login;
+import com.example.loginandregister.model.register.Register;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+    
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<Login> loginResponse(
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<Register> registerResponse(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("name") String name
+    );
+}
---
+++ app/src/main/java/com/example/loginandregister/model/login/Login.java
@@ -0,0 +1,49 @@
+package com.example.loginandregister.model.login;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Login{
+
+	@SerializedName("loginData")
+	private LoginData loginData;
+
+	@SerializedName("message")
+	private String message;
+
+	@SerializedName("status")
+	private boolean status;
+
+	public void setData(LoginData loginData){
+		this.loginData = loginData;
+	}
+
+	public LoginData getData(){
+		return loginData;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public void setStatus(boolean status){
+		this.status = status;
+	}
+
+	public boolean isStatus(){
+		return status;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Login{" + 
+			"loginData = '" + loginData + '\'' +
+			",message = '" + message + '\'' + 
+			",status = '" + status + '\'' + 
+			"}";
+		}
+}
